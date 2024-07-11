bacfdb983e8fee636282806041dddcdc888b1e68
Ramzizarg <ramzi.zargelayoun@esprit.tn>
2024-07-11 11:19:17 +0100

    Spring

---
+++ src/app/login/AuthService.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8089/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
+  }
+
+  isUserStudent(): boolean {
+    return true;
+  }
+
+  isUserTeacher(): boolean {
+    return false;
+  }
+}
---
+++ src/app/services/reservation-service/reservation-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-service',
+  standalone: true,
+  imports: [],
+  templateUrl: './reservation-service.component.html',
+  styleUrl: './reservation-service.component.css'
+})
+export class ReservationServiceComponent {
+
+}
